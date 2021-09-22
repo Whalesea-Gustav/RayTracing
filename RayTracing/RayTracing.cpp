98fa739ff56e4b49e03a2fec108a11ba84d74558
gustav <40590729+Whalesea-Gustav@users.noreply.github.com>
2021-09-22 14:47:12 +0800

    add refraction to material.h

--- RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing.cpp
@@ -42,7 +42,7 @@ color ray_color(const ray& r, const hittable& world, int depth)
 int main()
 {
     //Image
-    ofstream out("sphere wit material.ppm", ios::out);
+    ofstream out("full glass sphere with Schlick Approximation.ppm", ios::out);
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int> (image_width / aspect_ratio);
@@ -53,13 +53,14 @@ int main()
     //World
     hittable_list world;
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     //setting Camera
