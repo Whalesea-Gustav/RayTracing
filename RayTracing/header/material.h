f554e38f299ee8e73e91f18b130b3f3300f7ac6d
gustav <40590729+Whalesea-Gustav@users.noreply.github.com>
2021-09-23 12:33:00 +0800

    final result: ray tracing in one weekend.ppm time consumption sucks

--- RayTracing/header/material.h
+++ RayTracing/header/material.h
@@ -73,13 +73,14 @@ public :
     {
         attenuation = color(1.0, 1.0, 1.0);
         double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
         vec3 unit_direction = unit_vector(r_in.direction());
         double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
-        double sin_theta = sqrt(1.0 - cos_theta);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
 
-        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        bool cannot_refract = refraction_ratio * sin_theta - 1.0 > 0.0001;
         vec3 direction;
-        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) - random_double() > 0.0001)
         {
             direction = reflect(unit_direction, rec.normal);
         }
@@ -97,6 +98,7 @@ private:
         // Use Schlick's approximation for reflectance.
         auto r0 = (1 - ref_idx) / (1 + ref_idx);
         r0 = r0 * r0;
+        //return 1.0;
         return r0 + (1 - r0) * pow((1 - cosine), 5);
     }
 };
